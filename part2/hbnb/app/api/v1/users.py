0686c956375714c9dbce0490077f76af0f708ded
clementgibot25 <10500@holbertonstudents.com>
2025-06-18 16:21:50 +0200

    create request to create, update, get user

--- part2/hbnb/app/api/v1/users.py
+++ part2/hbnb/app/api/v1/users.py
@@ -12,6 +12,17 @@ user_model = api.model('User', {
 
 @api.route('/')
 class UserList(Resource):
+    @api.response(200, 'Success')
+    def get(self):
+        """Retrieve a list of all users"""
+        users = facade.get_all_users()
+        return [{
+            'id': user.id,
+            'first_name': user.first_name,
+            'last_name': user.last_name,
+            'email': user.email
+        } for user in users], 200
+
     @api.expect(user_model, validate=True)
     @api.response(201, 'User successfully created')
     @api.response(400, 'Email already registered')
@@ -44,3 +55,25 @@ class UserResource(Resource):
         if not user:
             return {'error': 'User not found'}, 404
         return {'id': user.id, 'first_name': user.first_name, 'last_name': user.last_name, 'email': user.email}, 200
+        
+    @api.expect(user_model, validate=True)
+    @api.response(200, 'User successfully updated')
+    @api.response(400, 'Email already registered')
+    @api.response(404, 'User not found')
+    def put(self, user_id):
+        """Update an existing user"""
+        user_data = api.payload
+        
+        # Vérifier si l'utilisateur existe
+        user = facade.get_user(user_id)
+        if not user:
+            return {'error': 'User not found'}, 404
+            
+        # Vérifier si l'email est modifié et s'il existe déjà
+        if 'email' in user_data and user_data['email'] != user.email:
+            existing_user = facade.get_user_by_email(user_data['email'])
+            if existing_user and existing_user.id != user_id:
+                return {'error': 'Email already registered'}, 400
+                
+        updated_user = facade.update_user(user_id, **user_data)
+        return {'id': updated_user.id, 'first_name': updated_user.first_name, 'last_name': updated_user.last_name, 'email': updated_user.email}, 200
