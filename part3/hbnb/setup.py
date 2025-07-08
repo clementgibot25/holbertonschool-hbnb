4d31caa4381fab5aa40b17effa737298130f94c1
clementgibot25 <10500@holbertonstudents.com>
2025-07-08 15:06:10 +0200

    create setup file

---
+++ part3/hbnb/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hbnb",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        'flask',
+        'flask-restx',
+        'flask-sqlalchemy',
+        'flask-bcrypt',
+        'flask-jwt-extended',
+        'python-dotenv',
+    ],
+)
