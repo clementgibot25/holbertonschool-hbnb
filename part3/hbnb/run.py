e6de7dacdbb959695ff1754411cfbb857f9cfcf1
clementgibot25 <10500@holbertonstudents.com>
2025-07-08 15:57:30 +0200

    add import of config

--- part3/hbnb/run.py
+++ part3/hbnb/run.py
@@ -3,8 +3,9 @@ Main application entry point for the HBnB API server.
 """
 
 from app import create_app
+from app.config import DevelopmentConfig
 
-app = create_app()
+app = create_app(DevelopmentConfig)
 
 if __name__ == '__main__':
     # Server configuration
